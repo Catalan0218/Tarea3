1c096a2ad247d3889a33ec01f4c53b32bfd82e1b
Diego <diego.catalan0218@gmail.com>
2022-12-22 12:17:00 -0600

    se agrego scritp para implementar funcion de boton

--- script/script.js
+++ script/script.js
@@ -37,14 +37,52 @@ const paises = [
     }
 ];
 
-
+let botonMostrar = document.getElementById('mostrarPaises');
+let botonRegistrar = document.getElementById('guardaRegistro');
 
 agregarMonitores();
 //funcion para agregar listeners a los botones
 function agregarMonitores() {
-    
+    botonRegistrar.addEventListener("click", function(){agregarPais();});
+    botonMostrar.addEventListener("click", function(){mostrarPaises();});
 }
 
 //funcion que mostrara las paises en la pagina
 function mostrarPaises() {
+    let paisRes = '';
+    for(let i = 0; i < paises.length; i++){
+        paisRes += `
+        <div class="col-12 col-lg-12 pt-3">
+            <div class="table-responsive">
+                <table class="table table-striped table-bordered table-sm">
+                    <thead class="table-dark">
+                        <tr>
+                            <th>Nombre</th>
+                            <th>Capital</th>
+                            <th>Idioma</th>
+                            <th>Moneda</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>${paises[i].nombre}</td>
+                            <td>${paises[i].capital}</td>
+                            <td>${paises[i].idioma_oficial}</td>
+                            <td>${paises[i].moneda}</td>
+                        </tr>
+                        <tr>
+                            <td colspan="4" class="text-center"><img src="${paises[i].bandera}" alt="bandera"></td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+        `;
+    }
+    document.getElementById("paises").innerHTML = paisRes;
+}
+
+//funcion que permite agregar un pais al arreglo
+function agregarPais() {
+   //No logre q funcionara :( JAJA
 }
